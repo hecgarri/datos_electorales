186c3dfecada05ab2547317e0f5f071a65f76258
hecgarri <hectorgarridohenriquez@gmail.com>
2017-09-23 22:46:26 -0300

    Actualizo carpeta

--- analisis_CEP_80.R
+++ analisis_CEP_80.R
@@ -1,2 +1,12 @@
 library(foreign)
-cep <- read.spss("~/GoogleDrivePersonal/Política/Datos/datos_electorales/Encuesta CEP 80 Jul-Ago 2017 v1.sav")
+file = "~/GoogleDrivePersonal/Política/Datos/datos_electorales/Encuesta CEP 80 Jul-Ago 2017 v1.sav"
+cep = read.spss(file)
+
+labels = attr(cep, "variable.labels")
+
+directorio = cbind(variables = names(cep),
+                   description = labels)
+
+rownames(directorio) = NULL
+
+cep = data.frame(cep)
